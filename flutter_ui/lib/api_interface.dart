633ff336302dc14fa9abf4911d2afe1baf747a49
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-04-27 20:44:59 +0530

    added support ofr android devices and minor bug fixes

--- flutter_ui/lib/api_interface.dart
+++ flutter_ui/lib/api_interface.dart
@@ -6,13 +6,26 @@ import 'bridge_generated.dart';
 // Initialize Rust API
 late final RustImpl api;
 
+// linux
+// void initRustApi() {
+//   try {
+//     final dylibPath = '/home/ibrahim/code/Rust_lang/encrypt_notepad/rust/target/release/librust.so';
+//     final dylib = DynamicLibrary.open(dylibPath);
+
+//     api = RustImpl(dylib);
+//     print('Rust dynamic library loaded successfully from $dylibPath.');
+//   } catch (e) {
+//     print('Failed to load Rust dynamic library: $e');
+//     rethrow;
+//   }
+// }
+
+//android
 void initRustApi() {
   try {
-    final dylibPath = '/home/ibrahim/code/Rust_lang/encrypt_notepad/rust/target/release/librust.so';
-    final dylib = DynamicLibrary.open(dylibPath);
-
+    final dylib = DynamicLibrary.open("librust.so");
     api = RustImpl(dylib);
-    print('Rust dynamic library loaded successfully from $dylibPath.');
+    print('Rust dynamic library loaded successfully.');
   } catch (e) {
     print('Failed to load Rust dynamic library: $e');
     rethrow;
